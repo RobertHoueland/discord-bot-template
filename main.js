24f65909b8d5ef4bdbf31effcf4428aaa9708e6c
Robert Houeland <71792920+TheRobdog@users.noreply.github.com>
2021-05-22 21:10:33 -0700

    change tab space width to 4 from 3

--- main.js
+++ main.js
@@ -9,240 +9,245 @@ console.log('USING PREFIX: "' + prefix + '"\n')
 
 // List of commands for help
 var commandsArr = [
-   "**help** (or commands) - shows this menu",
-   "**command1** - links gif",
-   "**command2** - links mp4 video",
-   "**command3** - links image\n",
-   "**voice** - joins voice channel and plays sound effect",
-   "**leave** - leaves voice channel",
-   "**ping** - checks bot ping",
+    "**help** (or commands) - shows this menu",
+    "**command1** - links gif",
+    "**command2** - links mp4 video",
+    "**command3** - links image\n",
+    "**voice** - joins voice channel and plays sound effect",
+    "**leave** - leaves voice channel",
+    "**ping** - checks bot ping",
 ]
 
 // Combine all commands into array to be printed
 function printArr(arr) {
-   var stringArr = ""
-   for (i = 0; i < arr.length; i++) {
-      stringArr += "\n" + prefix + arr[i]
-   }
-   return stringArr
+    var stringArr = ""
+    for (i = 0; i < arr.length; i++) {
+        stringArr += "\n" + prefix + arr[i]
+    }
+    return stringArr
 }
 
 function commandSuccess(msg) {
-   msg.react("✅")
-   if (msg.channel.type === "dm") {
-      console.log(msg.content + " was used by " + msg.author.tag)
-      return
-   } // Don't try to delete message in DM
-   setTimeout(() => msg.delete(), 3000)
-   // Delete user's message 3 seconds after using command
-   console.log(msg.content + " was used by " + msg.member.user.tag)
+    msg.react("✅")
+    if (msg.channel.type === "dm") {
+        console.log(msg.content + " was used by " + msg.author.tag)
+        return
+    } // Don't try to delete message in DM
+    setTimeout(() => msg.delete(), 3000)
+    // Delete user's message 3 seconds after using command
+    console.log(msg.content + " was used by " + msg.member.user.tag)
 }
 
 client.on("message", (msg) => {
-   if (msg.author == client.user || msg.author.bot) {
-      return // Prevent bot from replying to itself or other bot
-   }
-   if (msg.content.startsWith(prefix)) {
-      doCommand(msg)
-   }
+    if (msg.author == client.user || msg.author.bot) {
+        return // Prevent bot from replying to itself or other bot
+    }
+    if (msg.content.startsWith(prefix)) {
+        doCommand(msg)
+    }
 })
 
 function doCommand(msg) {
-   let command = msg.content.substr(1).toLowerCase() // Remove prefix
+    let command = msg.content.substr(1).toLowerCase() // Remove prefix
 
-   if (command == "help" || command == "commands") {
-      commandSuccess(msg)
-      const embed = new Discord.MessageEmbed()
-         .setColor("#3b3e42")
-         .setTitle("Robdog Bot")
-         .setAuthor("Created by Robdog#6848", "https://i.imgur.com/qfsnOig.jpg")
-         .setDescription("**Current Commands:**\n" + printArr(commandsArr))
-         .setThumbnail("INSERT IMAGE")
-         .setTimestamp()
-         .setImage("INSERT IMAGE")
-      msg.author.send(embed)
-   } else if (command == "command1") {
-      commandSuccess(msg)
-      msg.channel.send("INSERT GIF")
-   } else if (command == "command2") {
-      commandSuccess(msg)
-      msg.channel.send("INSERT MP4")
-   } else if (command == "command3") {
-      commandSuccess(msg)
-      msg.channel.send("INSERT IMAGE")
-   } else if (command == "ping") {
-      commandSuccess(msg)
-      msg.channel.send("Pinging...").then((sent) => {
-         sent.edit(`Ping: ${sent.createdTimestamp - msg.createdTimestamp}ms`)
-      })
-   }
+    if (command == "help" || command == "commands") {
+        commandSuccess(msg)
+        const embed = new Discord.MessageEmbed()
+            .setColor("#3b3e42")
+            .setTitle("Robdog Bot")
+            .setAuthor(
+                "Created by Robdog#6848",
+                "https://i.imgur.com/qfsnOig.jpg"
+            )
+            .setDescription("**Current Commands:**\n" + printArr(commandsArr))
+            .setThumbnail("INSERT IMAGE")
+            .setTimestamp()
+            .setImage("INSERT IMAGE")
+        msg.author.send(embed)
+    } else if (command == "command1") {
+        commandSuccess(msg)
+        msg.channel.send("INSERT GIF")
+    } else if (command == "command2") {
+        commandSuccess(msg)
+        msg.channel.send("INSERT MP4")
+    } else if (command == "command3") {
+        commandSuccess(msg)
+        msg.channel.send("INSERT IMAGE")
+    } else if (command == "ping") {
+        commandSuccess(msg)
+        msg.channel.send("Pinging...").then((sent) => {
+            sent.edit(`Ping: ${sent.createdTimestamp - msg.createdTimestamp}ms`)
+        })
+    }
 }
 
 var inVoice = 0
 client.on("message", async (msg) => {
-   if (msg.author == client.user || msg.author.bot) {
-      return // Prevent bot from replying to itself or other bot
-   }
+    if (msg.author == client.user || msg.author.bot) {
+        return // Prevent bot from replying to itself or other bot
+    }
 
-   if (msg.content.toLowerCase() === prefix + "voice") {
-      if (!msg.guild) {
-         msg.author.send("Voice commands can not be used in DMs")
-         console.log(
-            "Voice command was not run, not in server, used by " +
-               msg.author.tag
-         )
-         return
-      } // Voice commands can only be run in servers
-      commandSuccess(msg)
-      if (msg.member.voice.channel) {
-         const connection = await msg.member.voice.channel.join()
-         const dispatcher = connection.play("INSERT MP3")
-         inVoice = 1
-      } else {
-         console.log("Voice command was not run, user not in voice channel")
-         msg.channel.send("You need to join a voice channel!").then((sent) => {
-            let id = sent.id
-            msg.channel.messages
-               .fetch(id)
-               .then((message) => {
-                  message.delete({ timeout: 5000 })
-                  // Delete bot message after 5 seconds
-               })
-               .catch(console.error)
-         })
-      }
-   }
-   if (msg.content.toLowerCase() === prefix + "leave") {
-      if (!msg.guild) {
-         msg.author.send("Voice commands can not be used in DMs")
-         console.log(
-            "Voice command was not run, not in server, used by " +
-               msg.author.tag
-         )
-         return
-      } // Voice commands can only be run in servers
-      if (inVoice == 1) {
-         commandSuccess(msg)
-         msg.member.voice.channel.leave()
-         inVoice = 0
-      } else {
-         commandSuccess(msg)
-         console.log("Leave voice was not run, user not in voice channel")
-         msg.channel.send("Bot is not in a voice channel!").then((sent) => {
-            let id = sent.id
-            msg.channel.messages
-               .fetch(id)
-               .then((message) => {
-                  message.delete({ timeout: 5000 })
-                  // Delete bot message after 5 seconds
-               })
-               .catch(console.error)
-         })
-      }
-   }
+    if (msg.content.toLowerCase() === prefix + "voice") {
+        if (!msg.guild) {
+            msg.author.send("Voice commands can not be used in DMs")
+            console.log(
+                "Voice command was not run, not in server, used by " +
+                    msg.author.tag
+            )
+            return
+        } // Voice commands can only be run in servers
+        commandSuccess(msg)
+        if (msg.member.voice.channel) {
+            const connection = await msg.member.voice.channel.join()
+            const dispatcher = connection.play("INSERT MP3")
+            inVoice = 1
+        } else {
+            console.log("Voice command was not run, user not in voice channel")
+            msg.channel
+                .send("You need to join a voice channel!")
+                .then((sent) => {
+                    let id = sent.id
+                    msg.channel.messages
+                        .fetch(id)
+                        .then((message) => {
+                            message.delete({ timeout: 5000 })
+                            // Delete bot message after 5 seconds
+                        })
+                        .catch(console.error)
+                })
+        }
+    }
+    if (msg.content.toLowerCase() === prefix + "leave") {
+        if (!msg.guild) {
+            msg.author.send("Voice commands can not be used in DMs")
+            console.log(
+                "Voice command was not run, not in server, used by " +
+                    msg.author.tag
+            )
+            return
+        } // Voice commands can only be run in servers
+        if (inVoice == 1) {
+            commandSuccess(msg)
+            msg.member.voice.channel.leave()
+            inVoice = 0
+        } else {
+            commandSuccess(msg)
+            console.log("Leave voice was not run, user not in voice channel")
+            msg.channel.send("Bot is not in a voice channel!").then((sent) => {
+                let id = sent.id
+                msg.channel.messages
+                    .fetch(id)
+                    .then((message) => {
+                        message.delete({ timeout: 5000 })
+                        // Delete bot message after 5 seconds
+                    })
+                    .catch(console.error)
+            })
+        }
+    }
 })
 
 client.on("voiceStateUpdate", (oldState, newState) => {
-   // If number of users change in voice channel after setTimeout, have bot leave
-   if (
-      oldState.channelID !== oldState.guild.me.voice.channelID ||
-      newState.channel
-   ) {
-      return
-   }
-   if (!oldState.channel.members.size - 1)
-      setTimeout(() => {
-         if (!oldState.channel.members.size - 1) {
-            oldState.channel.leave()
-            console.log("Leaving voice channel after timeout")
-         }
-      }, 30000) // 30 second timer
+    // If number of users change in voice channel after setTimeout, have bot leave
+    if (
+        oldState.channelID !== oldState.guild.me.voice.channelID ||
+        newState.channel
+    ) {
+        return
+    }
+    if (!oldState.channel.members.size - 1)
+        setTimeout(() => {
+            if (!oldState.channel.members.size - 1) {
+                oldState.channel.leave()
+                console.log("Leaving voice channel after timeout")
+            }
+        }, 30000) // 30 second timer
 })
 
 var twitterVar = new Twit({
-   consumer_key: process.env.TWITTER_CONSUMER_KEY,
-   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-   access_token: process.env.TWITTER_ACCESS_TOKEN,
-   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 
-   strictSSL: true,
+    strictSSL: true,
 })
 
 function isReply(tweet) {
-   // No tweet alerts for replies
-   if (
-      tweet.retweeted_status ||
-      tweet.in_reply_to_status_id ||
-      tweet.in_reply_to_status_id_str ||
-      tweet.in_reply_to_user_id ||
-      tweet.in_reply_to_user_id_str ||
-      tweet.in_reply_to_screen_name
-   ) {
-      return true
-   }
-   return false
+    // No tweet alerts for replies
+    if (
+        tweet.retweeted_status ||
+        tweet.in_reply_to_status_id ||
+        tweet.in_reply_to_status_id_str ||
+        tweet.in_reply_to_user_id ||
+        tweet.in_reply_to_user_id_str ||
+        tweet.in_reply_to_screen_name
+    ) {
+        return true
+    }
+    return false
 }
 
 client.once("ready", () => {
-   console.log("BOT READY")
-   client.user.setActivity(prefix + "help")
+    console.log("BOT READY")
+    client.user.setActivity(prefix + "help")
 
-   var stream = twitterVar.stream("statuses/filter", {
-      follow: [process.env.TWITTER_USER_ID],
-   })
+    var stream = twitterVar.stream("statuses/filter", {
+        follow: [process.env.TWITTER_USER_ID],
+    })
 
-   stream.on("tweet", function (tweet) {
-      var url =
-         "https://twitter.com/" +
-         tweet.user.screen_name +
-         "/status/" +
-         tweet.id_str
-      console.log("New tweet, URL: " + url)
-      if (isReply(tweet) == true) {
-         console.log("Did not post reply to tweet")
-         return
-      }
-      try {
-         let channel = client.channels
-            .fetch(process.env.DISCORD_CHANNEL_ID)
-            .then((channel) => {
-               console.log("Alerting new tweet")
-               channel.send("**New Tweet:**\n\n" + url)
-            })
-            .catch((err) => {
-               console.log(err)
-            })
-      } catch (error) {
-         console.error(error)
-      }
-   })
+    stream.on("tweet", function (tweet) {
+        var url =
+            "https://twitter.com/" +
+            tweet.user.screen_name +
+            "/status/" +
+            tweet.id_str
+        console.log("New tweet, URL: " + url)
+        if (isReply(tweet) == true) {
+            console.log("Did not post reply to tweet")
+            return
+        }
+        try {
+            let channel = client.channels
+                .fetch(process.env.DISCORD_CHANNEL_ID)
+                .then((channel) => {
+                    console.log("Alerting new tweet")
+                    channel.send("**New Tweet:**\n\n" + url)
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        } catch (error) {
+            console.error(error)
+        }
+    })
 })
 
 const streamedRecently = new Set()
 client.on("presenceUpdate", (oldPresence, newPresence) => {
-   if (!newPresence.activities || streamedRecently.has(newPresence.user.tag)) {
-      // Don't alert about same user streaming twice in a row
-      return
-   }
-   newPresence.activities.forEach((activity) => {
-      if (activity.type == "STREAMING") {
-         console.log(
-            `${newPresence.user.tag} has started streaming at ${activity.url}`
-         )
-         let channel = client.channels
-            .fetch(process.env.TWITCH_CHANNEL_ID)
-            .then((channel) => {
-               channel.send(
-                  `**${newPresence.user.username} has started streaming!**\n\nCheck them out at ${activity.url}`
-               )
-            })
-         streamedRecently.add(newPresence.user.tag)
-         setTimeout(() => {
-            // Removes the user from the set after timer
-            streamedRecently.delete(newPresence.user.tag)
-         }, 14400000) // 4 hour cooldown on each person's stream notif
-      }
-   })
+    if (!newPresence.activities || streamedRecently.has(newPresence.user.tag)) {
+        // Don't alert about same user streaming twice in a row
+        return
+    }
+    newPresence.activities.forEach((activity) => {
+        if (activity.type == "STREAMING") {
+            console.log(
+                `${newPresence.user.tag} has started streaming at ${activity.url}`
+            )
+            let channel = client.channels
+                .fetch(process.env.TWITCH_CHANNEL_ID)
+                .then((channel) => {
+                    channel.send(
+                        `**${newPresence.user.username} has started streaming!**\n\nCheck them out at ${activity.url}`
+                    )
+                })
+            streamedRecently.add(newPresence.user.tag)
+            setTimeout(() => {
+                // Removes the user from the set after timer
+                streamedRecently.delete(newPresence.user.tag)
+            }, 14400000) // 4 hour cooldown on each person's stream notif
+        }
+    })
 })
 
 client.on("error", console.error)
